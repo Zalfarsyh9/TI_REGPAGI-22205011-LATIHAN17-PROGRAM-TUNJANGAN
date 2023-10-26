da9cc851e27f0ec8fb795b6df184aca3a89f7aa8
Zalfarsyh9 <145966652+Zalfarsyh9@users.noreply.github.com>
2023-10-26 11:46:00 +0700

    Add files via upload

---
+++ ProgramTunjanganJava.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.program.tunjangan.java;
+import java.util.Scanner;
+/**
+ *
+ * @author User
+ */
+public class ProgramTunjanganJava {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        //Input gaji pokok//
+        System.out.println("========Program Tunjangan========");
+        System.out.print("Berapa gaji pokok anda perbulan?: Rp. ");
+        double gajiPokok = scanner.nextDouble();
+
+        //Input status pernikahan
+        System.out.print("Status Anda? (Menikah/Belum) :");
+        String statusMenikah = scanner.next();
+
+        //Input tunjangan
+        double tunjangan = 0;
+        if (statusMenikah.equalsIgnoreCase("Menikah")) {
+            tunjangan = gajiPokok * 0.35;
+        }
+
+        //Tampilkan Tunjangan
+        double totalGaji = gajiPokok + tunjangan;
+
+        System.out.println("--------Hasil Perhitungan Gaji Anda------");
+        System.out.println("Gaji Pokok : Rp. " + gajiPokok);
+        System.out.println("Tunjangan : Rp. " + tunjangan);
+        System.out.println("Total Gaji : Rp. " + totalGaji);
+        
+    }
+}
